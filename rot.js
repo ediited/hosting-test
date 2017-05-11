47a28f2465c7e7d3d0d89f341f5c762714e369ea
Lauritz Kramberger <lauritz.kramberger@gmail.com>
2017-05-11 21:20:21 +0200

    alter

--- rot.js
+++ rot.js
@@ -17,10 +17,12 @@ function draw(){
   
 
 
-  textMode(CENTER,CENTER);
+  textAlign(CENTER,CENTER);
   fill(0,0,0);
   text(rotationX+"x, "+rotationY+"y, "+rotationZ+"z, ", window.innerWidth/2 , window.innerHeight-30);
+  
 
   fill(Math.abs(rotationX),Math.abs(rotationY),Math.abs(rotationZ));
+  
   rect(0,0,window.innerWidth,window.innerHeight);
 }
