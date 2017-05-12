945adc01585c858b9a593c4fb3760934ead81b19
Lauritz Kramberger <lauritz.kramberger@gmail.com>
2017-05-12 13:37:30 +0200

    jetzt?

--- cam.js
+++ cam.js
@@ -5,15 +5,28 @@ function setup(){
 
 
   capture = createCapture(VIDEO);
-  capture.size(window.innerWidth, window.innerHeight);
-
+  //capture.size(window.innerWidth, window.innerHeight);
+  capture.hide();
 
 
 
 }
 
 function draw(){
-image(capture,0,0,window.innerWidth,window.innerHeight);
+background(255);
+capture.loadPixels();
+  var stepSize = round(constrain(mouseX / 8, 6, 32));
+  for (var y=0; y<height; y+=stepSize) {
+    for (var x=0; x<width; x+=stepSize) {
+      var i = y * width + x;
+      var darkness = (255 - capture.pixels[i*4]) / 255;
+      var radius = stepSize * darkness;
+      ellipse(x, y, radius, radius);
+    }
+  }
+
+
+//image(capture,0,0,window.innerWidth,window.innerHeight);
 }
 
 
